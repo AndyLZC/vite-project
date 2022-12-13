da21adcd97b1113827131115af14a8741fa6a175
zc l <andylzce@gmail.com>
2022-12-13 18:43:02 +0800

    nginx

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div>About page</div>
+  <div>666</div>
   <button @click="handelAdd">inc</button>
   <button @click="handelDec">dec</button>
   <router-link to="/">to Home</router-link>
